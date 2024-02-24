ee908a23f8272319d794203df5b3ed0c6f566533
guntursatya25 <guntursatya812@gmail.com>
2024-02-24 11:19:37 +0700

    test

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -11,7 +11,7 @@ export default function ErrorPage() {
       <NavbarComponent />
       <div id="error-page">
         <h1>Oops!</h1>
-        <p>Sorry, an unexpected error has occurred.</p>
+        <p>Sorry, an unexpected error has occurred</p>
         <p>
           <i>{error.statusText || error.message}</i>
         </p>
